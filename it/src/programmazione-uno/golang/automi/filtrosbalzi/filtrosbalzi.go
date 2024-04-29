ab5d028f7c9a4ce858edef0deb8d28a64f696842
Giulio <giupersu@yahoo.it>
2024-04-29 09:59:50 +0200

    Iniziare revisione filtrosblazi

--- it/src/programmazione-uno/golang/automi/filtrosbalzi/filtrosbalzi.go
+++ it/src/programmazione-uno/golang/automi/filtrosbalzi/filtrosbalzi.go
@@ -1,93 +1,117 @@
-/**
-Creative Commons Legal Code
+/*
+	Creative Commons Legal Code
 
-CC0 1.0 Universal
+	CC0 1.0 Universal
 
-    CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE
-    LEGAL SERVICES. DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN
-    ATTORNEY-CLIENT RELATIONSHIP. CREATIVE COMMONS PROVIDES THIS
-    INFORMATION ON AN "AS-IS" BASIS. CREATIVE COMMONS MAKES NO WARRANTIES
-    REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS
-    PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM
-    THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED
-    HEREUNDER.package main
+CREATIVE COMMONS CORPORATION IS NOT A LAW FIRM AND DOES NOT PROVIDE
+LEGAL SERVICES. DISTRIBUTION OF THIS DOCUMENT DOES NOT CREATE AN
+ATTORNEY-CLIENT RELATIONSHIP. CREATIVE COMMONS PROVIDES THIS
+INFORMATION ON AN "AS-IS" BASIS. CREATIVE COMMONS MAKES NO WARRANTIES
+REGARDING THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS
+PROVIDED HEREUNDER, AND DISCLAIMS LIABILITY FOR DAMAGES RESULTING FROM
+THE USE OF THIS DOCUMENT OR THE INFORMATION OR WORKS PROVIDED
+HEREUNDER.package main
 */
 package main
 
+/*
+#include <stdio.h>
+
+int getChar() {
+	return getchar();
+}
+*/
+import "C"
+
 import (
 	"fmt"
+	"os"
 )
 
-func checkInput(s string, r rune) {
+func gotoQ0() {
+
+	fmt.Print("0")
+	r := rune(C.getChar())
+
+	// Transita allo stato q0
+	if r == '0' {
+		gotoQ0()
+	}
+
+	// Transita allo stato q1
+	if r == '1' {
+		gotoQ1()
+	}
+
+	// Transita allo stato finale q4
+	gotoQ4()
+}
+
+func gotoQ1() {
+
+	fmt.Print("0")
+	r := rune(C.getChar())
+
+	// Transita allo stato q0
+	if r == '0' {
+		gotoQ0()
+	}
+
+	// Transita allo stato q2
+	if r == '1' {
+		gotoQ2()
+	}
+
+	// Transita allo stato finale q4
+	gotoQ4()
+}
+
+func gotoQ2() {
+
+	fmt.Print("1")
+	r := rune(C.getChar())
+
+	// Transita allo stato q3
+	if r == '0' {
+		gotoQ3()
+	}
+
+	// Transita allo stato q2
+	if r == '1' {
+		gotoQ2()
+	}
+
+	// Transita allo stato finale q4
+	gotoQ4()
+}
+
+func gotoQ3() {
+
+	fmt.Print("1")
+	r := rune(C.getChar())
+
+	// Transita allo stato q0
+	if r == '0' {
+		gotoQ0()
+	}
+
+	// Transita allo stato q2
+	if r == '1' {
+		gotoQ2()
+	}
+
+	// Transita allo stato finale q4
+	gotoQ4()
 
 }
 
-func automaBounceFilter(w string) bool {
+func gotoQ4() {
 
-	currentState := 'a' // stato iniziale
-	for _, char := range w {
-		// Esegue la transizione in base allo stato corrente e al carattere letto
-		switch currentState {
-		case 'a':
-			if char == '0' {
-				currentState = 'a'
-				fmt.Print("0")
-			} else if char == '1' {
-				currentState = 'b'
-				fmt.Print("0")
-			}
-		case 'b':
-			if char == '0' {
-				currentState = 'a'
-				fmt.Print("0")
-			} else if char == '1' {
-				currentState = 'c'
-				fmt.Print("1")
-			}
-		case 'c':
-			if char == '0' {
-				currentState = 'd'
-				fmt.Print("1")
-			} else if char == '1' {
-				currentState = 'c'
-				fmt.Print("1")
-			}
-		case 'd':
-			if char == '0' {
-				currentState = 'a'
-				fmt.Print("0")
-			} else if char == '1' {
-				currentState = 'c'
-				fmt.Print("1")
-			}
-		}
-	}
-	// Verifica se lo stato finale è accettante
-	fmt.Println()
-	return currentState == 'c'
-
+	os.Exit(1)
 }
 
 func main() {
 
-	var words = []string{
-		"",
-		"0",
-		"01",
-		"010",
-		"0101",
-		"1010",
-		"0011",
-		"01011",
-		"010110",
-		"0101101",
-	}
-
-	for _, str := range words {
-		if automaBounceFilter(str) {
-			fmt.Printf("'%s' OK\n", str)
-		} else {
-			fmt.Printf("'%s' NO\n", str)
-		}
-	}
+	gotoQ0()
+	fmt.Println()
 }
